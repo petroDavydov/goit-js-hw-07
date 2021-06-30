d7caea4300c8937bb10159f446c838ab36fbdfaf
petroDavydov <petrodavydov@gmail.com>
2021-06-30 15:47:28 +0300

    add 4 task

---
+++ js/task-04.js
@@ -0,0 +1,54 @@
+// Счетчик состоит из спана и кнопок, которые должны увеличивать и уменьшать
+// значение счетчика на `1`.
+
+// - Создай переменную `counterValue` в которой будет хранится текущее значение
+//   счетчика.
+// - Создай функции `increment` и `decrement` для увеличения и уменьшения значения
+//   счетчика
+// - Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
+
+// const counter = {
+
+//   counterValue: 0,
+
+//   increment() {
+//     this.counterValue += 1;
+//   },
+
+//   decrement() {
+//     this.counterValue -= 1;
+//   },
+// };
+
+// const decrementMinus = document.querySelector('[data-action="decrement"]');
+// const incrementPlus = document.querySelector('[data-action="increment"]');
+// const showValue = document.querySelector("#value");
+
+// incrementPlus.addEventListener("click", () => {
+//   counter.increment();
+//   showValue.textContent = counter.counterValue;
+// });
+
+// decrementMinus.addEventListener("click", () => {
+//   counter.decrement();
+//   showValue.textContent = counter.counterValue;
+// });
+
+const btnIncrement = document.querySelector('[data-action="increment"]');
+const btnDecrement = document.querySelector('[data-action="decrement"]');
+const value = document.querySelector("#value");
+
+let counterValue = 0;
+
+const decrement = () => {
+  counterValue -= 1;
+  value.textContent = counterValue;
+};
+
+const increment = () => {
+  counterValue += 1;
+  value.textContent = counterValue;
+};
+
+btnIncrement.addEventListener("click", increment);
+btnDecrement.addEventListener("click", decrement);
