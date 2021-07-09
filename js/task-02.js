a0b51979920a97d85026084fbae3c7e3e977cd5c
petroDavydov <petrodavydov@gmail.com>
2021-07-09 12:29:15 +0300

    add second variant

--- js/task-02.js
+++ js/task-02.js
@@ -7,7 +7,23 @@ const ingredients = [
   "Приправы",
 ];
 
-const ingredientsEl = ingredients.forEach((ingredient) => {
+// const ingredientsEl = ingredients.forEach((ingredient) => {
+//   const listIngredient = document.querySelector("#ingredients");
+
+//   let createTegLi = document.createElement("li");
+
+//   createTegLi.append(ingredient);
+
+//   //   console.log(createTegLi.append(ingredient));
+
+//   const fullIngredientsList = listIngredient.appendChild(createTegLi);
+//   //   console.log(fullIngredientsList);
+//   return fullIngredientsList;
+// });
+
+// ==2 variant==
+
+const ingredientsEl = ingredients.map((ingredient) => {
   const listIngredient = document.querySelector("#ingredients");
 
   let createTegLi = document.createElement("li");
@@ -20,3 +36,18 @@ const ingredientsEl = ingredients.forEach((ingredient) => {
   //   console.log(fullIngredientsList);
   return fullIngredientsList;
 });
+
+//   ==3 variant==найден подобний код но не понят! зачем, там ведь список...
+
+// const ingredientsEl = ingredients.forEach((ingredient) => {
+
+// 	const listIngredient = document.querySelector("#ingredients");
+
+// 	let createTegLi = document.createElement("li");
+
+// 	createTegLi.innerHTML = ingredient;
+// 	listIngredient.append(createTegLi);
+// 	parent.append(...createTegLi)
+//   });
+
+//   console.log(ingredientsEl);
